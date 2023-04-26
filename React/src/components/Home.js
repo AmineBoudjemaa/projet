fb668ca5dba9872f81fe9964685f78a2fbc12165
AmineBoudjemaa <aminboudj30@gmail.com>
2023-04-26 23:35:28 +0100

    'signUp'

--- React/src/components/Home.js
+++ React/src/components/Home.js
@@ -4,12 +4,16 @@ import Landing from './homeComponents/Landing';
 import ComSoon from './homeComponents/ComSoon';
 import Teachers from './homeComponents/Teachers';
 import axios from 'axios';
+const api = axios.create({
+  baseURL: "http://localhost:3000",
+  withCredentials: true, // send cookies with requests
+});
 
 
 export default class Home extends Component {
   submitHandler = (e) => {
-    axios
-      .get("http://localhost:3000/auth/me")
+    api
+      .get("/auth/me")
       .then((res) => {
         console.log(res);
       })
